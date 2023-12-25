cca56c24dbc9e170f757b11be38178a29def6e04
karunashreem <shreekarunam@gmail.com>
2023-12-25 11:13:18 +0530

    Update sum_of_a_series.java

--- sum_of_a_series.java
+++ sum_of_a_series.java
@@ -6,3 +6,16 @@ class Solution {
         return sum;
     }
 }
+
+//Takes too much time
+// class Solution {
+//     // function to return sum of  1, 2, ... n
+//     long seriesSum(int n) {
+//         // code here
+//         int sum=0;
+//         for(int i=0; i<=n; i++){
+//             sum=sum+i;
+//         }
+//         return sum;
+//     }
+// }
