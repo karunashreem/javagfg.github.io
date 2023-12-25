48d74854d6e2687fb67bfa94cb14afb4fe6e76a1
karunashreem <shreekarunam@gmail.com>
2023-12-25 11:31:11 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,17 @@
+class GfG
+{
+    void printPat(int n)
+    {
+         // Your code here
+          for (int i=n; i>=1; i--){
+             for (int j=n; j>0; j-- ){
+                 for (int k=1; k<=i; k++){
+                     System.out.print(j+" ");
+                 }
+                 
+             }
+              System.out.print("$");
+         }
+        
+    }
+}
