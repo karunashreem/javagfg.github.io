641c2a6d2b2803a1f62b441023167174c5d638f9
karunashreem <shreekarunam@gmail.com>
2023-12-25 11:16:36 +0530

    Create alternate_element_of_an_array.java

---
+++ alternate_element_of_an_array.java
@@ -0,0 +1,11 @@
+class GfG
+{
+    public static void print(int arr[], int n)
+    {
+        // your code here
+        for (int i=0; i<n; i++){
+            System.out.print(arr[i]+ " ");
+            i++;
+        }
+    }
+}
