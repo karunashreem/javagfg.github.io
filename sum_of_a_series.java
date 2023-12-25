d3c15208c9d9626aa6f78bfe55414585db762674
karunashreem <shreekarunam@gmail.com>
2023-12-25 11:11:33 +0530

    Create sum_of_a_series.java

---
+++ sum_of_a_series.java
@@ -0,0 +1,8 @@
+class Solution {
+    // function to return sum of  1, 2, ... n
+    long seriesSum(int n) {
+        // code here
+        long sum = ((long)n*(n+1))/2;
+        return sum;
+    }
+}
