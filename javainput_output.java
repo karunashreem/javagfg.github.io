3747053eb67194161a66ad5be524ee06f243dabd
karunashreem <shreekarunam@gmail.com>
2023-12-29 02:50:23 +0530

    Create javainput_output.java

---
+++ javainput_output.java
@@ -0,0 +1,8 @@
+class Solution{
+    static void printIntString(String S,int N){
+        // code here
+        System.out.println("The input string :"+ S);
+        System.out.println("The input integer :"+ N);
+        
+    }
+}
