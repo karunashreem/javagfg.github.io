b75f74b133ea0fbd80b8ddd9694597991ef88afc
karunashreem <shreekarunam@gmail.com>
2023-12-31 19:37:05 +0530

    Create switch_case_statement.java

---
+++ switch_case_statement.java
@@ -0,0 +1,19 @@
+class Solution{
+    static double switchCase(int choice, List<Double> arr){
+        // code here
+        double x=0, y=0;
+        switch(choice){
+        case 1:
+            x = ((arr.get(0)*arr.get(0))*Math.PI);
+                break;
+        case 2:
+            y =(arr.get(0)*arr.get(1));
+                break;
+        }
+         if(choice ==1){
+            return x;
+        }else{
+            return y;
+        }
+    }
+}
