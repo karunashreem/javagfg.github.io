ca966ed004a85c1352c88f9b2179c1d03f9a9637
karunashreem <shreekarunam@gmail.com>
2024-01-02 12:33:48 +0530

    Create java_datatypes.java

---
+++ java_datatypes.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        // code here
+        return sc.nextInt();
+    }
+    
+    String javaStringType(Scanner sc) {
+        // code here
+        return sc.next();
+    }
+    
+    float javaFloatType(Scanner sc) {
+        // code here
+        return sc.nextFloat();
+    }
+}
