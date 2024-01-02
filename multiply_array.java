1744700bcfddc84b1791cce9f500ef5d0fbae6f0
karunashreem <shreekarunam@gmail.com>
2024-01-02 12:33:44 +0530

    Create multiply_array.java

---
+++ multiply_array.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    public static int product(int arr[],int n)
+    {
+        int mul= 1;
+        for (int i=0; i<n; i++){
+            mul*=arr[i];
+        }
+        return mul;
+    }
+}
