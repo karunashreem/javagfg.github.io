8f09e0c84d2fdaf96b1e2d8bd3e01eaf77bfbecd
karunashreem <shreekarunam@gmail.com>
2024-01-10 00:45:34 +0530

    Create binary_array_sorting.java

---
+++ binary_array_sorting.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    //Function to sort the binary array.
+    static void binSort(int A[], int N)
+    {
+        // add your code here
+        Arrays.sort(A);
+        
+        
+        /**************
+        * No need to print the array
+        * ************/
+    }
+}
