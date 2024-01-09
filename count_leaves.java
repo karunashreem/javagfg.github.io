822c08f36990c5938c50a4387d0793e5de9ba904
karunashreem <shreekarunam@gmail.com>
2024-01-10 00:45:38 +0530

    Create count_leaves.java

---
+++ count_leaves.java
@@ -0,0 +1,9 @@
+class Tree
+{
+    int countLeaves(Node node) 
+    {
+         // Your code  
+         if(node==null) return 0;
+        if(node.left==null && node.right==null) return 1;
+         return countLeaves(node.left)+countLeaves(node.right);}
+    }
