0e76d5dd3496182da6d87bbfed3928b48319dd7c
karunashreem <shreekarunam@gmail.com>
2024-01-16 19:36:36 +0530

    Create pattern1basic.java

---
+++ pattern1basic.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    void printSquare(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
