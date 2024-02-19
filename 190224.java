6b0cc94c5d605ba0b22d924ef7db078751648be1
karunashreem <shreekarunam@gmail.com>
2024-02-19 19:11:21 +0530

    Create 190224.java

---
+++ 190224.java
@@ -0,0 +1,11 @@
+
+class BinaryTree
+{
+    static int sumBT(Node head){
+        //Code
+        if(head== null){
+            return 0;
+        }
+        return head.data + sumBT(head.left) + sumBT(head.right);
+    }
+}
