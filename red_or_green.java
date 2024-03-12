721bb387bd33fd7cc0f8e6194d2281b1fe977e85
karunashreem <shreekarunam@gmail.com>
2024-03-12 08:48:37 +0530

    Create red_or_green.java

---
+++ red_or_green.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    static int RedOrGreen(int N, String S) {
+        //code here
+        int countR=0;
+        int countG=0;
+        for(int i=0;i<N;i++){
+            if(S.charAt(i)=='R'){
+                countR+=1;
+            }
+            else{
+                countG+=1;
+            }
+        }
+        if(countR<countG){
+            return countR;
+        }
+        
+        return countG;
+    }
+}
