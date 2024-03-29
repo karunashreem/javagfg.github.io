c29c336f798c546653fb016637c7aa954656bf14
karunashreem <shreekarunam@gmail.com>
2024-03-29 09:11:34 +0530

    Create pattern_#.java

---
+++ pattern_#.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    void printTriangle(int n) {
+        int k=1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+    }
+}
+}
