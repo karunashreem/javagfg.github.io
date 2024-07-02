a447d6aa070f879dd80cf06e58715d7dff099e54
karunashreem <shreekarunam@gmail.com>
2024-07-02 23:28:45 +0530

    Create swap_the_bit.java

---
+++ swap_the_bit.java
@@ -0,0 +1,11 @@
+class Solution {
+    
+    public long printMinimumProduct(long arr[], long n)
+    {
+        Arrays.sort(arr);
+
+        long ans=arr[0]*arr[1];
+
+        return ans;
+    }
+}
