fbf3f3a299d8aa42b1581f2f30717083a686228f
M Karuna Shree <127692384+karunashreem@users.noreply.github.com>
2024-07-06 23:01:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Message Spreading/message-spreading.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.minimumMessages(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int minimumMessages(int N){
+        // code here
+        return N*2-2; 
+    }
+}
